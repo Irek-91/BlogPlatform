635f22456653aaea95659986e7b8ac04c2c8e95b
Irek Shamilov <shamilov.irek@bk.ru>
2023-07-08 15:42:47 +0300

    mongoDb & vercel

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express, {Request, Response} from 'express'
-import { postType } from './type';
 import { postsRouter} from './routes/posts-router';
 import { blogsRouter} from './routes/blogs-router';
-import { blogsRepository } from './repositories/blogs-repository';
-import { postRepository } from './repositories/post-repository';
+import { blogsRepository } from './repositories/blogs-in-memory-repository';
+import { postRepository } from './repositories/post-in-memory-repository';
+import { runDb } from './db/db-mongo';
 
 const app = express()
 const port = 3001
@@ -14,13 +14,18 @@ app.use(express.json())
 app.use('/posts', postsRouter)
 app.use('/blogs', blogsRouter)
 
-app.delete('/testing/all-data', async (req: Request, res: Response) => {
+/*app.delete('/testing/all-data', async (req: Request, res: Response) => {
   await blogsRepository.deleteBlogAll();
   await postRepository.deletePostAll();
 
   res.sendStatus(204)
-}) 
+})
+*/ 
 
-app.listen(port, () => {
+const startApp = async () => {
+  await runDb()
+  app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+}
+startApp()
