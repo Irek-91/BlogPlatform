e96b49b395e2540a272110668203ecad874c1ba7
Irek Shamilov <shamilov.irek@bk.ru>
2023-07-15 19:04:16 +0300

    fix

--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -13,11 +13,11 @@ blogsRouter.get('/',
   
   async (req: Request, res: Response) => {
   const searchNameTerm: string = req.body.searchNameTerm;
-  const sortBy: string = req.body.sortBy;
-  const sortDirection: any = req.body.sortDirection;
-  const pageNumber: number = +req.body.pageNumber;
-  const pageSize: number = +req.body.pageSize;
-  if (sortDirection === "asc") {const sortDirection = 1} else {const sortDirection = -1}
+  const sortBy: string = req.body.sortBy || "createdAt";
+  let sortDirection: 1 | -1 = 1;
+  const pageNumber: number = +req.body.pageNumber || 1;
+  const pageSize: number = +req.body.pageSize || 10;
+  if (req.body.sortDirection === "asc") {sortDirection = 1} else {sortDirection = -1}
 
   const foundBlogs = await blogsService.findBlogs(searchNameTerm, sortBy, sortDirection, pageNumber, pageSize);
   res.send(foundBlogs)
