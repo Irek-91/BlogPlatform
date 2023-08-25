883c51c35b718ac3d488f9aeedabd9de24b18f4f
Irek Shamilov <shamilov.irek@bk.ru>
2023-08-25 21:25:38 +0300

    fix

--- src/midlewares/count-IPAndURIFilter.ts
+++ src/midlewares/count-IPAndURIFilter.ts
@@ -14,7 +14,7 @@ export const filterCountIPAndURL = async (req: Request, res: Response, next: Nex
     }
 
     const count = await arrayIPAndURICollections.countDocuments({date: {$gte: new Date((newAPI.date).getTime() - 10000)}})
-    if (count > 5) {return res.sendStatus(429)}
+    if (count >= 5) {return res.sendStatus(429)}
     const result = await arrayIPAndURICollections.insertOne({...newAPI})
 
     next()
