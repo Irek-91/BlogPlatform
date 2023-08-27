39d2780ee7b5d768a66aeda6b985d1d7e32e65ff
Irek Shamilov <shamilov.irek@bk.ru>
2023-08-27 22:44:16 +0300

    fix

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -19,7 +19,6 @@ export const authRouter = Router({});
 
 authRouter.post('/login',
     loginOrEmailValidationAuth,
-    loginValidation,
     passwordValidationAuth,
 
     inputValidationMiddleware,
